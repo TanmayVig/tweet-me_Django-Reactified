6d68d76f85e59ac56ed1fdb607ff06f31ae8e18a
Tanmay Vig <tanmayvij11@gmail.com>
2020-12-29 00:56:37 +0530

    started to shift on restframework

--- tweetme/settings.py
+++ tweetme/settings.py
@@ -27,6 +27,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = ["127.0.0.1","mydomain.com"]
 LOGIN_URL = "/login"
+MAX_TWEET_LENGTH = 240
 
 # Application definition
 
@@ -37,6 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # third party
+    'rest_framework',
+    # internal
     'tweets',
 ]
 
@@ -121,4 +125,17 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# modify django rest api
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.SessionAuthentication',
+        # 'rest_framework.authentication.BasicAuthentication'
+    ],
+    'DEFAULT_RENDERER_CLASSES': [
+        'rest_framework.renderers.JSONRenderer',
+        # 'rest_framework.renderers.BrowsableAPIRenderer',
+    ]
+}
 
--- tweets/views.py
+++ tweets/views.py
@@ -1,21 +1,58 @@
+import json
 import random
 from django.views.decorators.csrf import csrf_exempt
 from tweetme.settings import ALLOWED_HOSTS
 from django.utils.http import is_safe_url
+
+from rest_framework.authentication import SessionAuthentication
+from rest_framework.decorators import api_view, permission_classes, authentication_classes
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
 from django.conf import settings
 from django.http.response import Http404
 from django.shortcuts import render, redirect
 from django.http import HttpResponse, Http404, JsonResponse
 from . models import Tweet
 from .forms import TweetForm
+from .serializers import TweetSerializer
 
 ALLOWED_HOSTS = settings.ALLOWED_HOSTS
 
 def home_view(request, *args, **kwargs):
     return render(request,'pages/home.html',context={},status=200)
 
-# @csrf_exempt
+@api_view(['POST']) # http method allowed
+# @authentication_classes([SessionAuthentication])
+@permission_classes([IsAuthenticated])
 def tweet_create_view(request, *args, **kwargs):
+    data = request.POST
+    serializer = TweetSerializer(data=data)
+    if serializer.is_valid(raise_exception=True):
+        serializer.save(user = request.user)
+        return Response(serializer.data, status= 201)
+
+    return Response({}, status= 400)
+
+@api_view(['GET'])
+def tweet_list_view(request, *args, **kwargs):
+    qs = Tweet.objects.all()
+    serializer = TweetSerializer(qs, many= True)
+    return Response(serializer.data)
+
+@api_view(['GET'])
+def tweet_detail_view(request, *args, **kwargs):
+    qs = Tweet.objects.filter(id = tweet_id)
+    if not qs:
+        return Response({}, status=404)
+    obj = qs.first()
+    serializer = TweetSerializer(obj)
+    return Response(serializer.data,status=200)
+
+
+
+
+# @csrf_exempt
+def tweet_create_view_pure_django(request, *args, **kwargs):
     user = request.user
     if not request.user.is_authenticated:
         user = None
@@ -43,7 +80,7 @@ def tweet_create_view(request, *args, **kwargs):
             return JsonResponse(form.errors,status = 400)
     return render(request, 'components/form.html',context={'form':form})
 
-def tweet_list_view(request, *args, **kwargs):
+def tweet_list_view_pure_django(request, *args, **kwargs):
     qs = Tweet.objects.all()
     tweets_list = [x.serialize() for x in qs]
     data = {
@@ -52,7 +89,7 @@ def tweet_list_view(request, *args, **kwargs):
     }
     return JsonResponse(data)
 
-def tweet_detail_view(request, tweet_id, *args, **kwargs):
+def tweet_detail_view_pure_django(request, tweet_id, *args, **kwargs):
     '''
     REST API VIEW
     return json data
